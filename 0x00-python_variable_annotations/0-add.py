7945cd83adb3e685b589595f0f33ddbeddcd919f
moyasser20 <mohamedyasser192023@gmail.com>
2024-08-02 19:22:39 +0300

    task1 done

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+'''
+    Basic annotations for variables.
+'''
+
+def add(a:float , b:float) -> float:
+    '''
+        Add two floats and return a float.
+    '''
+    return a + b
